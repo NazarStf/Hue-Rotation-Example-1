1562df2174649589555d5e3b71f3a5875f24272b
NazarStf <hworker98@gmail.com>
2023-07-27 22:20:25 +0300

    Initial Commit

---
+++ Hue Rotation Example 1/Hue_Rotation_Example_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hue_Rotation_Example_1App.swift
+//  Hue Rotation Example 1
+//
+//  Created by NazarStf on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hue_Rotation_Example_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
